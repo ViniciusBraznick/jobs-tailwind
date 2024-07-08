65b861b5abaa001af8d6d09bd68e8fac53f704c3
Vinicius Braznick <viniciusbraznick@gmail.com>
2024-07-08 20:03:09 -0300

    hotfix: fix z-index value in search component

--- src/view/pages/Dashboard/components/Search.tsx
+++ src/view/pages/Dashboard/components/Search.tsx
@@ -11,8 +11,8 @@ export function Search() {
         </button>
       </Dialog.Trigger>
       <Dialog.Portal>
-        <Dialog.Overlay className="bg-black/45 backdrop-blur-sm	 data-[state=open]:animate-overlayShow fixed inset-0" />
-        <Dialog.Content className="data-[state=open]:animate-contentShow fixed top-[20%] left-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white shadow-md focus:outline-none">
+        <Dialog.Overlay className="bg-black/45 backdrop-blur-sm data-[state=open]:animate-overlayShow fixed inset-0 z-10" />
+        <Dialog.Content className="data-[state=open]:animate-contentShow fixed top-[20%] left-[50%] max-h-[85vh] w-[90vw] z-20 max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white shadow-md focus:outline-none">
           <form>
             <div className="flex items-center w-full">
               <Input className="w-full border-none" name="Buscar" placeholder="Pesquisar por..."/>
